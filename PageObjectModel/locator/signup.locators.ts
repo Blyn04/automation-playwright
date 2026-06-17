41312ac06e5d8fce84194e3c4a84d4b687a5e1aa
berlene <berlenebernabe12@gmail.com>
2026-06-17 19:06:42 +0800

    feat: sign up automation

---
+++ PageObjectModel/locator/signup.locators.ts
@@ -0,0 +1,10 @@
+export const SignUpPageLocators = {
+  NAME_INPUT: 'input[name="name"]',
+  EMAIL_INPUT: 'input[name="email"]',  
+  EMPLOYEE_ID_INPUT: 'input[placeholder="e.g., 12-3456"]',
+  JOB_TITLE_DROPDOWN: 'select[name="jobTitle"]',
+  DEPARTMENT_DROPDOWN: 'select[name="department"]',
+  TERMS_CHECKBOX: 'input[name="termsChecked"]',
+  SIGN_UP_BUTTON: 'button[type="submit"]',
+  GO_TO_SIGN_UP_BUTTON: '//span[contains(text(),"Sign up here")]'
+};
--- fixtures/AuthFixtures.ts
+++ fixtures/AuthFixtures.ts
@@ -1,13 +1,20 @@
 import { test as baseTest } from '@playwright/test';
 import { LoginPage } from '../PageObjectModel/pages/LoginPage';
+import { SignUpPage } from '../PageObjectModel/pages/SignUpPage';
 
 type AuthFixtures = {
   loginPage: LoginPage;
+  signUpPage: SignUpPage;
 };
 
 export const test = baseTest.extend<AuthFixtures>({
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await use(loginPage);
-    }, 
+  },
+  
+  signUpPage: async ({ page }, use) => {
+    const signUpPage = new SignUpPage(page);
+    await use(signUpPage);
+  },
 });
